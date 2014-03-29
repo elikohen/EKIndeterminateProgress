6cc2372bebf132e1064bb23112555e11a80dc184
Eli Kohen Gomez <elikohen@Elis-MacBook-Pro.local>
2014-03-29 19:38:15 +0100

    Added documentation

--- EKIndeterminateProgress/EKIndeterminateProgressView.h
+++ EKIndeterminateProgress/EKIndeterminateProgressView.h
@@ -8,22 +8,56 @@
 
 #import <UIKit/UIKit.h>
 
+/** Animations to be applied */
 typedef NS_OPTIONS(NSInteger, EKIndeterminateProgressAnimation){
+    /** Horizontal translation */
     EKIndeterminateProgressAnimationTranslate = 1 << 0,
+    /** Clockwise rotation */
     EKIndeterminateProgressAnimationRotate
 };
 
+/**
+ *  Indeterminate progress View, alternative to UIActivityIndicator
+ */
 @interface EKIndeterminateProgressView : UIView
 
+/**
+ *  Determines wether to hide entire view then stopped and wether to unhide when started.
+ */
 @property (nonatomic) BOOL hidesWhenStopped;
+/**
+ *  Determines the paddings to the edges of the view where the progresses will be moving.
+ */
 @property (nonatomic) UIEdgeInsets edgeInsets;
+/**
+ *  Animation duration (each loop)
+ */
 @property (nonatomic) CGFloat duration;
+/**
+ *  Animations to be applied (i.e: EKIndeterminateProgressAnimationTranslate|EKIndeterminateProgressAnimationRotate)
+ */
 @property (nonatomic) NSInteger animations;
 
+/**
+ *  Initializes view using the background as size and starting at point 0,0
+ *
+ *  @param background UIImage to be set as View background
+ *  @param progresses Array of progresses that will be randomly used as progress when progress started
+ *
+ *  @return EKIndeterminateProgressView
+ */
 - (instancetype) initWithBackground: (UIImage*) background andProgress: (NSArray*) progresses;
 - (instancetype) initWithFrame:(CGRect)frame background: (UIImage*) background andProgresses: (NSArray*) progresses;
-
+/**
+ *  Starts animating, and unhides if hidesWhenStopped
+ */
 - (void) startAnimating;
+/**
+ *  Stops animating, and hides if hidesWhenStopped
+ */
 - (void) stopAnimating;
+/**
+ *  Retuns YES if animating
+ */
 - (BOOL) animating;
 @end
---
+++ README.md
@@ -0,0 +1,8 @@
+EKIndeterminateProgressView
+===========================
+
+Animated view that takes a background and a list of progresses. Will animate a random picked progress from the list each time startAnimating is called.
+
+Full documentation here:
+[Documentation](/Documentation/index.html)
+
